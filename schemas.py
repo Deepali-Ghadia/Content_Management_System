5f67778754dea03ed5ab9f701fa4d48e4dd11236
Deepali-Ghadia <deepalinghadia1606@gmail.com>
2023-03-18 13:18:09 +0530

    defined schemas for view_all_users and create_user APIs

---
+++ schemas.py
@@ -0,0 +1,37 @@
+from pydantic import BaseModel
+from typing import Optional
+
+class CreateUser(BaseModel):
+    name: str
+    username: str
+    mobile_number: int
+    email_id: str
+    password: str
+    
+    class Config:
+        orm_mode = True
+    
+    
+class UserResponse(BaseModel):
+    id: int
+    name: str
+    username: str
+    mobile_number: int
+    email_id: str
+    password: str
+    role: str
+    profile_photo: Optional[int] = None
+    bio: Optional[str] = None
+    
+    class Config:
+        orm_mode = True
+        
+        
+class UpdateUser(BaseModel):
+    name: str
+    username: str
+    mobile_number: int
+    email_id: str
+    password: str
+    profile_photo: Optional[int] = None
+    bio: Optional[str] = None
