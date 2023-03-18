f3a8aab33fbdec65796a1bbdb4d3c5d259146a0b
Deepali-Ghadia <deepalinghadia1606@gmail.com>
2023-03-18 22:21:36 +0530

    schemas for CRUD on Post

--- schemas.py
+++ schemas.py
@@ -1,10 +1,11 @@
 from pydantic import BaseModel
-from typing import Optional
+from typing import Optional, Dict
+from datetime import datetime
 
 class CreateUser(BaseModel):
     name: str
     username: str
-    mobile_number: int
+    mobile_number: str
     email_id: str
     password: str
     
@@ -16,7 +17,7 @@ class UserResponse(BaseModel):
     id: int
     name: str
     username: str
-    mobile_number: int
+    mobile_number: str
     email_id: str
     password: str
     role: str
@@ -29,7 +30,48 @@ class UserResponse(BaseModel):
         
 class UpdateUser(BaseModel):
     username: str
-    mobile_number: int
+    mobile_number: str
     password: str
     profile_photo: Optional[int] = None
     bio: Optional[str] = None
+    
+    class Config:
+        orm_mode = True
+  
+    
+    
+class CreatePost(BaseModel):
+    title: str
+    description: str
+
+    
+    class Config:
+        orm_mode = True
+    
+    
+class ShowAllPost(BaseModel):
+    id: int
+    title: str
+    description: str
+    # posted_on: datetime
+    is_featured: bool
+    is_published: bool
+    
+    class Config:
+        orm_mode = True
+        
+        
+class ShowPostByUser(BaseModel):
+    id: int
+    title: str
+    description: str
+    # posted_on: datetime
+    is_featured: bool
+    is_published: bool
+    # posted_by: Dict[UserResponse]
+    
+class UpdatePost(BaseModel):
+    title: str
+    description: str
+    # posted_on: datetime
+    is_published: bool
