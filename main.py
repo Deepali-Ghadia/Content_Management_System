498b4b6bd2898a426279d563b968fab9e2d36504
Deepali-Ghadia <deepalinghadia1606@gmail.com>
2023-03-18 22:21:06 +0530

    APIS for CRUD on Post without linking with the user table

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ def register_user(user: schemas.CreateUser):
         email_id= user.email_id,
         password= user.password
     )
-    
+
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
@@ -67,3 +67,47 @@ def delete_an_user(id: int):
     db.refresh(user_to_delete)
     return user_to_delete
     
+    
+    
+# view all posts
+@app.get('/posts', response_model=List[schemas.ShowAllPost], status_code=status.HTTP_200_OK)
+def get_all_posts():
+    posts = db.query(models.Post).all()
+    return posts
+
+
+# create a post 
+@app.post('/posts', response_model=schemas.ShowAllPost, status_code=status.HTTP_201_CREATED)
+def create_post(post: schemas.CreatePost):
+    new_post = models.Post(
+        title = post.title,
+        description = post.description 
+    )
+    db.add(new_post)
+    db.commit()
+    db.refresh(new_post)
+    return new_post
+
+# update a post
+@app.post('/posts/update/{id}', response_model=schemas.ShowPostByUser)
+def update_an_post(id: int, post:schemas.UpdatePost):
+    post_to_update = db.query(models.Post).filter(models.Post.id == id).first()
+    post_to_update.title = post.title
+    post_to_update.description = post.description
+    post_to_update.is_published = post.is_published
+    
+    db.commit()
+    db.refresh(post_to_update)
+    return post_to_update
+
+
+# delete a post
+@app.delete('/posts/delete/{id}', response_model=schemas.ShowPostByUser)
+def delete_an_post(id: int):
+    post_to_delete=db.query(models.Post).filter(models.Post.id==id).first()
+    if post_to_delete is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post does not exist")
+    db.delete(post_to_delete)
+    db.commit()
+    db.refresh(post_to_delete)
+    return post_to_delete
