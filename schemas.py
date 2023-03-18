352a03c88e002631ef5f07b079975c4defe36b39
Deepali-Ghadia <deepalinghadia1606@gmail.com>
2023-03-19 00:48:13 +0530

    schemas for fetching data from other User and showing in Post and vice-versa

--- schemas.py
+++ schemas.py
@@ -1,7 +1,30 @@
 from pydantic import BaseModel
-from typing import Optional, Dict
+from typing import Optional, List
 from datetime import datetime
 
+
+class ShowUser(BaseModel):
+    name: str
+    username: str
+    email_id: str
+    
+    class Config:
+        orm_mode=True  
+        
+        
+class ShowPostByUser(BaseModel):
+    id: int
+    title: str
+    description: str
+    # posted_on: datetime
+    is_featured: bool
+    is_published: bool
+    user: ShowUser # this is the relationship wala variable
+ 
+    class Config:
+        orm_mode = True
+        
+        
 class CreateUser(BaseModel):
     name: str
     username: str
@@ -13,6 +36,13 @@ class CreateUser(BaseModel):
         orm_mode = True
     
     
+class ShowPost(BaseModel):
+    title: str
+    description: str
+    
+    class Config:
+        orm_mode = True
+    
 class UserResponse(BaseModel):
     id: int
     name: str
@@ -21,6 +51,7 @@ class UserResponse(BaseModel):
     email_id: str
     password: str
     role: str
+    posts: List
     profile_photo: Optional[int] = None
     bio: Optional[str] = None
     
@@ -43,12 +74,15 @@ class UpdateUser(BaseModel):
 class CreatePost(BaseModel):
     title: str
     description: str
+    posted_by: int
 
-    
     class Config:
         orm_mode = True
     
     
+
+        
+         
 class ShowAllPost(BaseModel):
     id: int
     title: str
@@ -56,22 +90,20 @@ class ShowAllPost(BaseModel):
     # posted_on: datetime
     is_featured: bool
     is_published: bool
+    posted_by: int
+    user: ShowUser
     
     class Config:
         orm_mode = True
         
-        
-class ShowPostByUser(BaseModel):
-    id: int
-    title: str
-    description: str
-    # posted_on: datetime
-    is_featured: bool
-    is_published: bool
-    # posted_by: Dict[UserResponse]
+      
+
     
 class UpdatePost(BaseModel):
     title: str
     description: str
     # posted_on: datetime
     is_published: bool
+    
+    class Config:
+        orm_mode = True
