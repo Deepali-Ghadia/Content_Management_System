69c1c358e321907fb02860ecd43150664586e734
Deepali-Ghadia <deepalinghadia1606@gmail.com>
2023-03-25 02:20:36 +0530

    implemented authorisation in change_user_role functionality

--- routers/admin.py
+++ routers/admin.py
@@ -6,7 +6,7 @@ import schemas, models
 from routers.authentication import get_current_user
 
 
-router = APIRouter(prefix="/admin", tags=["Admin"])  
+router = APIRouter(prefix="/admin")  
     
 def validate_admin(current: int = Depends(get_current_user)):
     current_user = db.query(models.User).filter(models.User.id == current).first()    
@@ -14,7 +14,7 @@ def validate_admin(current: int = Depends(get_current_user)):
 
 
 # API to mark a post as featured post
-@router.post('/posts/mark_featured/{id}', response_model=schemas.ShowAllPost)
+@router.post('/posts/mark_featured/{id}', response_model=schemas.ShowAllPost, tags=["Admin"])
 def mark_featured(id: int, is_admin: str = Depends(validate_admin)):
     
     to_mark = db.query(models.Post).filter((models.Post.id == id) & (is_admin=="admin")).first()
@@ -22,7 +22,7 @@ def mark_featured(id: int, is_admin: str = Depends(validate_admin)):
         if to_mark.is_featured == False:
             to_mark.is_featured = True
     else:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="You don't have authority to update role of any user")
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="You don't have authority to mark a post as a featured post")
 
     db.commit()
     db.refresh(to_mark)
@@ -30,18 +30,25 @@ def mark_featured(id: int, is_admin: str = Depends(validate_admin)):
 
 
 
+
 # show all featured posts
-@router.get('/posts/show_featured/{value}', response_model=List[schemas.ShowFeaturedPosts])
+@router.get('/posts/show_featured/{value}', response_model=List[schemas.ShowFeaturedPosts], tags=["Posts"])
 def list_all_featured_posts(value: bool):
     featured_posts = db.query(models.Post).filter(models.Post.is_featured==value).all()
     return featured_posts
 
 
+
+
 # API to change user roles and permissions
-@router.post('/update_user_role/{id}', response_model=schemas.UserResponse)
-def change_user_role(id: int, user: schemas.ChangeRole):
-    user_details = db.query(models.User).filter(models.User.id == id).first()
-    user_details.role = user.role
+@router.post('/update_user_role/{id}', response_model=schemas.UserResponse, tags=["Admin"])
+def change_user_role(id: int, user: schemas.ChangeRole, is_admin: str = Depends(validate_admin)):
+    user_details = db.query(models.User).filter((models.User.id == id) & (is_admin=="admin")).first()
+    
+    if user_details is not None:
+        user_details.role = user.role
+    else:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="You don't have authority to update role of any user")
     
     db.commit()
     db.refresh(user_details)
