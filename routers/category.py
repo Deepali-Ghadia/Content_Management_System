58f6cdea189adbcb32af7a16a4661de6c0c3cb66
Deepali-Ghadia <deepalinghadia1606@gmail.com>
2023-03-24 23:21:13 +0530

    implemented authentication and authorization in category module

--- routers/category.py
+++ routers/category.py
@@ -1,57 +1,63 @@
-from fastapi import APIRouter, status, HTTPException
+from fastapi import APIRouter, status, HTTPException, Depends
 from database import db
 from passlib.context import CryptContext
 from typing import List
 import schemas, models
+from routers.authentication import get_current_user
 
 
-router = APIRouter()
+router = APIRouter(prefix="/category", tags=['Category'])
 
 
 # APIs for Category
 
 # create a category
-@router.post('/category', response_model=schemas.Category)
-def create_category(category: schemas.CreateCategory):
-    new_category = models.Category(
-        name= category.name
-    )
-    db.add(new_category)
-    db.commit()
-    db.refresh(new_category)
-    return new_category
+@router.post('/create_category', response_model=schemas.Category)
+def create_category(category: schemas.CreateCategory, random :int = Depends(get_current_user)):
+    if random is not None:
+        new_category = models.Category(
+            name= category.name
+        )
+        db.add(new_category)
+        db.commit()
+        db.refresh(new_category)
+        return new_category
     
     
-# View all categories along with posts
-@router.get('/categories', response_model=List[schemas.Category])
-def get_categorieswith_posts():
-    categories = db.query(models.Category).all()
-    return categories
+# # View all categories along with posts
+# @router.get('/view_all', response_model=List[schemas.Category])
+# def get_categorieswith_posts():
+#     categories = db.query(models.Category).all()
+#     return categories
 
 
 
 # list all categories
-@router.get('/categories_list/', response_model=List[schemas.ListCategory])
-def get_list_of_categories():
-    list_of_categories = db.query(models.Category).all()
-    return list_of_categories 
+@router.get('/list_all', response_model=List[schemas.ListCategory])
+def get_list_of_categories(random :int = Depends(get_current_user)):
+    if random is not None:
+        list_of_categories = db.query(models.Category).all()
+        return list_of_categories 
 
 
 
-# delete a category
-@router.delete('/categories/{id}', response_model=schemas.Category)
-def delete_a_category(id: int):
-    category_to_delete = db.query(models.Category).filter(models.Category.id == id).first()
-    if category_to_delete is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Category does not exist")
-    db.delete(category_to_delete)
-    db.commit()
-    db.refresh(category_to_delete)
-    return category_to_delete
+# # delete a category
+# @router.delete('/delete/{id}', response_model=schemas.Category)
+# def delete_a_category(id: int):
+#     category_to_delete = db.query(models.Category).filter(models.Category.id == id).first()
+#     if category_to_delete is None:
+#         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Category does not exist")
+#     db.delete(category_to_delete)
+#     db.commit()
+#     db.refresh(category_to_delete)
+#     return category_to_delete
     
 
+
+
 # API to get all the posts of a particular category
-@router.get('/posts/search/{category_id}', response_model=schemas.SearchByCategory)
-def search_by_category(category_id : int):
-    posts_of_a_category  = db.query(models.Category).filter(models.Category.id == category_id).first()
-    return posts_of_a_category
+@router.get('/search/{category_id}', response_model=schemas.SearchByCategory)
+def search_by_category(category_id : int, random :int = Depends(get_current_user)):
+    if random is not None:
+        posts_of_a_category  = db.query(models.Category).filter(models.Category.id == category_id).first()
+        return posts_of_a_category
