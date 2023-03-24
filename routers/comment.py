fdab2d01a4548f7a38f9cafad37a38fd80ddcc1a
Deepali-Ghadia <deepalinghadia1606@gmail.com>
2023-03-24 22:40:16 +0530

    implemented authentication and authorization in comment module

--- routers/comment.py
+++ routers/comment.py
@@ -1,36 +1,43 @@
-from fastapi import APIRouter, status, HTTPException
+from fastapi import APIRouter, status, HTTPException, Depends
 from database import db
 from passlib.context import CryptContext
 from typing import List
 import schemas, models
+from routers.authentication import get_current_user
 
 
-router = APIRouter()
+router = APIRouter(prefix="/comments",
+                   tags=["Comment"])
 
 
 #  APIs for Adding Comments
 # add comment to a particular post
-@router.post('/comments/{id}', response_model=schemas.ShowComment)
-def add_comment(id: int, comment: schemas.AddComment):
-    validate = db.query(models.Post).filter(models.Post.id == id).first()
-    if validate is not None: 
-        new_comment = models.Comment(
-            post_id = validate.id,
-            description =  comment.description,
-            commented_by = comment.commented_by
-        )
-    db.add(new_comment)
-    db.commit()
-    db.refresh(new_comment)
-    return new_comment
+@router.post('/add_comment/{id}', response_model=schemas.ShowComment)
+def add_comment(id: int, comment: schemas.AddComment, random:int = Depends(get_current_user)):
+    if random is not None:
+        validate = db.query(models.Post).filter((models.Post.id == id)).first()
+        if validate is not None: 
+            new_comment = models.Comment(
+                post_id = validate.id,
+                description =  comment.description,
+                commented_by = comment.commented_by
+            )
+        db.add(new_comment)
+        db.commit()
+        db.refresh(new_comment)
+        return new_comment
+
+
 
 
 # delete a comment by id
-@router.delete('/delete_comments/{id}')
-def delete_comment_by_id(id: int):
-    comment_to_delete = db.query(models.Comment).filter(models.Comment.id == id).first()
+@router.delete('/delete_comment/{id}')
+def delete_comment_by_id(id: int, user_id:int = Depends(get_current_user)):
+    comment_to_delete = db.query(models.Comment).filter((models.Comment.id == id) & (models.User.id == user_id)).first()
+    
     if comment_to_delete is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Comment does not exist")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Either the comment odes not exist or you are not allowed to delete this comment")
+    
     db.delete(comment_to_delete)
     db.commit()
     db.refresh(comment_to_delete)
