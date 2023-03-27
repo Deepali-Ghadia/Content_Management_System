4f83c9e7f49fe416cf1d5a29ae9709d0ec0eca4c
Deepali-Ghadia <deepalinghadia1606@gmail.com>
2023-03-27 20:10:13 +0530

    API to upload media files

--- routers/media_library.py
+++ routers/media_library.py
@@ -1,10 +1,10 @@
 import os, shutil
-from fastapi import APIRouter, UploadFile, File
+from fastapi import APIRouter, UploadFile, File, HTTPException, Depends, status
 from database import db
 from passlib.context import CryptContext
 from typing import List
 import schemas, models
-
+from routers.authentication import get_current_user
 
 router = APIRouter()
 
@@ -12,21 +12,24 @@ router = APIRouter()
 # APIs for Media Library
 
 # View all uploaded files
-@router.get('/media_files', response_model=List[schemas.ShowMediaFile])
-def show_all_media_files():
-    files = db.query(models.Media).all()
-    return files
+@router.get('/media_files', response_model=List[schemas.ShowMediaFile], tags=["Media Library"])
+def show_all_media_files(id: int = Depends(get_current_user)):
+    
+    user_media_files = db.query(models.Media).filter(models.Media.user == id).all()
+    
+    return user_media_files
 
 
 
 # upload a file and add it to local directory
-@router.post('/media_library', response_model=schemas.ShowMediaFile)
-def upload_file(file: UploadFile = File(...)):  
+@router.post('/media_library', response_model=schemas.ShowMediaFile, tags=["Media Library"])
+def upload_file(file: UploadFile = File(...), id:int = Depends(get_current_user)):  
     
-    upload_dir = r"E:\Shubhchintak Technology Pvt. Ltd. (Internship)\Project\Content Management System\Coding Workspace\Content Management System\uploaded_files"
+    upload_dir = r'E:/Shubhchintak Technology Pvt. Ltd. (Internship)/Project/Content Management System/Coding Workspace/Content Management System/upload_files/'
     
     # get the destination path
     destination_path = os.path.join(upload_dir, file.filename)
+    print(file.filename)
     print(destination_path)
     
     # copying file contents
@@ -34,18 +37,40 @@ def upload_file(file: UploadFile = File(...)):
         shutil.copyfileobj(file.file, buffer)
     # wb means write and binary
     
+    
     # # adding the locally stored file to the database
-    # db_file = db.query(models.Media).filter(models.Media.link == file.filename).first()
-    # if db_file is not None:
-    #     raise HTTPException(status_code=400, detail="Media File already exists")
-    
-    # print(file.filename)
-    # new_media = models.Media(
-    #     link = file.filename
-    # )
 
-    # db.add(new_media)
-    # db.commit()
-    # db.refresh(new_media)
+    db_file = db.query(models.Media).filter(models.Media.link == destination_path).first()
+    if db_file is not None:
+        raise HTTPException(status_code=400, detail="Media File already exists")
     
-    return {"Result": "OK"}
+
+    new_media = models.Media(
+        link = destination_path ,
+        user = id
+    )
+
+    db.add(new_media)
+    db.commit()
+    db.refresh(new_media)
+    
+    return new_media
+
+
+
+# delete media
+@router.delete("/media_library/delete/{id}", tags=["Media Library"])
+def delete_media_file(id: int, user_id:int = Depends(get_current_user)):
+    media_to_delete=db.query(models.Media).filter((models.Media.id==id) & (models.Media.user == user_id)).first()
+    
+    if media_to_delete is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Either the media file does not exist or you are not the owner of this media file")
+    
+    
+    db.delete(media_to_delete)
+    db.commit()
+    db.refresh(media_to_delete)
+    return media_to_delete
+    
+    
+    
