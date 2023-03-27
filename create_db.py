67c7556997b39546cda8a38f87f379274f74dab8
Deepali-Ghadia <deepalinghadia1606@gmail.com>
2023-03-27 20:08:55 +0530

    changed name of database

--- create_db.py
+++ create_db.py
@@ -5,4 +5,3 @@ print("Creating database...")
 # metadata.create_all creates a table for all the classes that we have provided in the models.py file. These classes are inherited from Base class and Base class is an instance of declarative_base()
 Base.metadata.create_all(engine)
 
-print("Database created successfully")
