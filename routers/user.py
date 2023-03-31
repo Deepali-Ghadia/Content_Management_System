8f5a0c8b6c72f65d610bdb1bb68a2a119afa4218
Deepali-Ghadia <deepalinghadia1606@gmail.com>
2023-03-31 11:36:05 +0530

    modified code to show message when a user is deleted instead of showing internal server error

--- routers/user.py
+++ routers/user.py
@@ -3,6 +3,7 @@ from database import db
 from typing import List
 import schemas, models
 from routers.authentication import get_current_user
+import sqlalchemy
 
 
 router = APIRouter(prefix="/user", tags=["User"])
@@ -30,7 +31,10 @@ def get_user_by_id(id: int, random: int = Depends(get_current_user)):
 @router.post('/update/', response_model=schemas.UserResponse, status_code=status.HTTP_200_OK)
 def update_user(user: schemas.UpdateUser, user_id: int = Depends(get_current_user)):
 
-
+    # profile_photo is 0\
+    if user.profile_photo == 0:
+        user.profile_photo = None
+        
     user_to_update = db.query(models.User).filter(models.User.id == user_id).first()
     user_to_update.username = user.username,
     user_to_update.mobile_number = user.mobile_number,
@@ -55,26 +59,29 @@ def delete_an_user(user_id: int = Depends(get_current_user)):
     # print("comments are ........\n")
     # print(posts_of_a_user[0].comments) # prints list of comments
  
-    for i in range(len(posts_of_a_user)):
-        for j in range(len(posts_of_a_user[i].comments)):
-            db.delete(posts_of_a_user[i].comments[j])
+        
+    try:
+        for i in range(len(posts_of_a_user)):
+            for j in range(len(posts_of_a_user[i].comments)):
+                db.delete(posts_of_a_user[i].comments[j])
         db.delete(posts_of_a_user[i])
 
-    print("Deleted all the posts along with comments")
+        print("Deleted all the posts along with comments")
     
-    
-    # delete all the comments made by me
-    db.query(models.Comment).filter(models.Comment.commented_by == user_id).delete()
-    print("deleted all the comments made by me")
-    
+        
+        # delete all the comments made by me
+        db.query(models.Comment).filter(models.Comment.commented_by == user_id).delete()
+        print("deleted all the comments made by me")
+        
 
-    # delete the user
-    user_to_delete=db.query(models.User).filter(models.User.id==user_id).first()
+        # delete the user
+        user_to_delete=db.query(models.User).filter(models.User.id==user_id).first()
 
-   
-    db.delete(user_to_delete)
-    print(6)
-    db.commit()
-    db.refresh(user_to_delete)
-    return {"User deleted successfully"}
     
+        db.delete(user_to_delete)
+        print(6)
+        db.commit()
+        db.refresh(user_to_delete)
+        
+    except sqlalchemy.exc.InvalidRequestError:
+        raise HTTPException(status_code=status.HTTP_200_OK, detail="User deleted successfully")
